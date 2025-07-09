5696a3ef31628b0da15130392f826f191196f04c
Asuma09 <241l002@andrew.ac.jp>
2025-07-10 00:13:03 +0900

    アニメーション

--- src/App.svelte
+++ src/App.svelte
@@ -1,59 +1,101 @@
 <script lang="ts">
-  let title = '';
-  let todoList: string[] = [];
+import { slide } from  'svelte/transition';
 
-  $: disabledCreateButton = title === '';
+const Priorities = {
+	High: 1,
+	Mid: 2,
+	Low: 3,
+} as const;
+type Priority = typeof Priorities[keyof typeof Priorities];
+
+const priorityNames = {
+	[Priorities.High]: 'High',
+	[Priorities.Mid]: 'Mid',
+	[Priorities.Low]: 'Low',
+} as const;
+type TodoItem = {
+	id: number;
+	title: string;
+	priority?: Priority;
+};
+
+let title = '';
+let priority: number = 2;
+let todoList: TodoItem[] = [];
+  
+$: disabledCreateButton = title === '';
+$: sortedTodoList = todoList.sort(
+	(item1, item2) => item1.priority - item2.priority
+);
 
   // 作成ボタンを押したときの処理
-  const handleClickCreateButton = () => {
-    todoList = [...todoList, title];
+const handleClickCreateButton = () => {
+const id = new Date().getTime();
+	if (priority === 1 || priority === 2 || priority === 3) {
+		todoList = [...todoList, { id, title, priority }];
+	}
     title = '';
-  };
+};
 
   // 完了処理
-  const completeItem = (index) => {
-    todoList = todoList.filter((_, i) => i !== index);
-  };
+const completeItem = (id: number) => {
+    todoList = todoList.filter((todo) => todo.id !== id);
+};
 </script>
 
-<div class="container mt=3">
-	<div class="row justify-content-center">
-		<div class="col-lg-6 col-mt-8">
-			<h2>TODO アプリ</h2>
-			<form>
-				<div class="form-group">
-					<label for="title">タイトル</label>
-					<input id="title" class="form-control" bind:value={title} />
-				</div>
-				<div class="form-group text-center">
-					<button
-					class="btn btn-primary px-5"
-					disabled={disabledCreateButton}
-					on:click={handleClickCreateButton}>
-					create
-					</button>
-				</div>
-			</form>
-		</div>
-	</div>
-	<div class="row justify-content-center">
-		<div class="cal-lg-6 cal-nt-8">
-			{#if todoList.length === 0}
-				<div>タスクなし</div>
-			{:else}
-				<ul class="list-group">
-					{#each todoList as todoItem, index}
-						<li class="list-group-item align-middle">
-							{todoItem}
-							<button
-								class="btn btn-sm btn-success float-right"
-								on:click={() => completeItem(index)}>
-								×
-							</button>
-						</li>
-					{/each}
-				</ul>
-			{/if}
-		</div>
-	</div>
+<div class="container mt-3">
+  <div class="row justify-content-center">
+    <div class="col-lg-6 col-md-8">
+      <h2>TODO アプリ</h2>
+      <form>
+        <div class="form-group">
+          <label for="title">タイトル</label>
+          <input id="title" class="form-control" bind:value={title} />
+        </div>
+        <div class="form-group">
+          <label for="priority">優先度</label>
+          <select id="priority" class="form-control" bind:value={priority}>
+            {#each Object.entries(priorityNames) as [value, label]}
+              <option value={Number(value)}>{label}</option>
+            {/each}
+          </select>
+        </div>
+        <div class="form-group text-center">
+          <button
+            class="btn btn-primary px-5"
+            disabled={disabledCreateButton}
+            on:click={handleClickCreateButton}>
+            作成
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
+  <div class="row justify-content-center">
+    <div class="col-lg-6 col-md-8">
+      {#if todoList.length === 0}
+        <div>アイテムを作成してください</div>
+      {:else}
+        <ul class="list-group">
+          {#each sortedTodoList as todoItem (todoItem.id)}
+            <li transition:slide class="list-group-item align-middle">
+              <span
+                class="badge m-1"
+                class:badge-danger={todoItem.priority === Priorities.High}
+                class:badge-warning={todoItem.priority === Priorities.Mid}
+                class:badge-success={todoItem.priority === Priorities.Low}>
+                {priorityNames[todoItem.priority]}
+              </span>
+              {todoItem.title}
+              <button
+                class="btn btn-sm btn-success float-right"
+                on:click={() => completeItem(todoItem.id)}>
+                ×
+              </button>
+            </li>
+          {/each}
+        </ul>
+      {/if}
+    </div>
+  </div>
 </div>
